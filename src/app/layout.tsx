ee6758bd01ecc2f255e08a3637cf55c0bf96621c
KingTang <ndohyannick78@gmail.com>
2026-01-28 09:28:18 +0100

    ajout des favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -65,6 +65,18 @@ export default function RootLayout({
             />
             <meta name="theme-color" content="#0a0e27" />
             <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=5" />
+            <link rel="icon" href="/favicon.ico" />
+
+            <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+            <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+
+            <link rel="icon" type="image/png" sizes="192x192" href="/android-chrome-192x192.png" />
+            <link rel="icon" type="image/png" sizes="512x512" href="/android-chrome-512x512.png" />
+
+            <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+
+            <link rel="manifest" href="/site.webmanifest" />
+
         </head>
         <body className="custom-scrollbar bg-dark-bg text-gray-100">
         <LanguageProvider>
